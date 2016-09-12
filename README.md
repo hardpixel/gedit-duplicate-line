2d85579fed4bc510511ccb035bb0d64e2558a530
jonian <jonian@hardpixel.eu>
2016-09-12 15:32:20 +0300

    add readme and license various updates

---
+++ README.md
@@ -0,0 +1,10 @@
+## Gedit Plugin: Duplicate Line
+
+Gedit 3 plugin to duplicate highlighted selections using `Ctrl+D`.
+
+# Installation
+
+1. Copy the plugin folder to `~/.local/share/gedit/plugins`
+2. Open gedit and click `Edit -> Preferences -> Plugins`
+3. Check the `Duplicate Line` and hit `Close`
+4. That's it! Now you can use `Ctrl+D` to duplicate the highlighted block.
--- duplicate_line.py
+++ duplicate_line.py
@@ -1,4 +1,10 @@
-from gi.repository import GObject, Gio, Gdk, Gtk, Gedit
+import gi
+
+gi.require_version('Gdk', '3.0')
+gi.require_version('Gtk', '3.0')
+gi.require_version('Gedit', '3.0')
+
+from gi.repository import GObject, Gdk, Gtk, Gedit
 
 class DuplicateLineWindowActivatable(GObject.Object, Gedit.WindowActivatable):
 
@@ -7,23 +13,25 @@ class DuplicateLineWindowActivatable(GObject.Object, Gedit.WindowActivatable):
 	def __init__(self):
 		GObject.Object.__init__(self)
 
+		self._handler_id = None
+
 	def do_activate(self):
-		self.handler_id = self.window.connect("key-press-event", self.on_key_press)
+		self._handler_id = self.window.connect("key-press-event", self.on_key_press)
 
 	def do_deactivate(self):
-		self.window.disconnect(self.handler_id)
+		self.window.disconnect(self._handler_id)
 
-	def on_key_press(self, term, event):
-		modifiers = event.state & Gtk.accelerator_get_default_mod_mask()
-
+	def on_key_press(self, _key, event):
 		if event.keyval in (Gdk.KEY_D, Gdk.KEY_d):
+			modifiers = event.state & Gtk.accelerator_get_default_mod_mask()
+
 			if modifiers == Gdk.ModifierType.CONTROL_MASK:
-				self.on_duplicate_line_key_press()
+				self.duplicate_selection()
 				return True
 
 		return False
 
-	def on_duplicate_line_key_press(self, action=None, user_data=None):
+	def duplicate_selection(self):
 		doc = self.window.get_active_document()
 		selection_iter = doc.get_selection_bounds()
 
